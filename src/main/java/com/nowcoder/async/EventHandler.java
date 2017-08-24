2dd44edc0daea145ee2fd1398ee360525d911c33
yuanhaiyangxq <yhy.xmu@foxmail.com>
2017-08-24 19:57:38 +0800

    Update EventHandler.java

--- src/main/java/com/nowcoder/async/EventHandler.java
+++ src/main/java/com/nowcoder/async/EventHandler.java
@@ -3,7 +3,7 @@ package com.nowcoder.async;
 import java.util.List;
 
 /**
- * Created by nowcoder on 2016/7/16.
+ * Created by nowcoder on 2017/8/16.
  */
 public interface EventHandler {
     void doHandle(EventModel model);
