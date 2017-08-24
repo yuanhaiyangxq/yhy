50f9e02a47d3c30ccae22e59317ba409372937fc
yuanhaiyangxq <yhy.xmu@foxmail.com>
2017-08-24 20:15:55 +0800

    Update CommentService.java

--- src/main/java/com/nowcoder/service/CommentService.java
+++ src/main/java/com/nowcoder/service/CommentService.java
@@ -9,7 +9,7 @@ import javax.websocket.server.ServerEndpoint;
 import java.util.List;
 
 /**
- * Created by nowcoder on 2016/7/9.
+ * Created by yhy on 2017/8/21.
  */
 @Service
 public class CommentService {
