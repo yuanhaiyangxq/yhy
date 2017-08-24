3a8d8c5061c093c8bffe63c7ffb7677469da63b4
yuanhaiyangxq <yhy.xmu@foxmail.com>
2017-08-24 19:57:01 +0800

    Update EventConsumer.java

--- src/main/java/com/nowcoder/async/EventConsumer.java
+++ src/main/java/com/nowcoder/async/EventConsumer.java
@@ -19,7 +19,7 @@ import java.util.List;
 import java.util.Map;
 
 /**
- * Created by nowcoder on 2016/7/16.
+ * Created by nowcoder on 2017/8/16.
  */
 @Service
 public class EventConsumer implements InitializingBean, ApplicationContextAware {
