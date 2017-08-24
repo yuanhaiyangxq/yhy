81e6f380513ce35d077a3f3be2114fd40733921a
yuanhaiyangxq <yhy.xmu@foxmail.com>
2017-08-24 20:18:09 +0800

    Update UserService.java

--- src/main/java/com/nowcoder/service/UserService.java
+++ src/main/java/com/nowcoder/service/UserService.java
@@ -14,7 +14,7 @@ import org.springframework.stereotype.Service;
 import java.util.*;
 
 /**
- * Created by nowcoder on 2016/7/2.
+ * Created by yhy on 2017/8/1.
  */
 @Service
 public class UserService {
