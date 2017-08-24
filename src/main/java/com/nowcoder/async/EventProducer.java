e7d9452e544979ed08bc50bcc1b21e838f3ddb40
yuanhaiyangxq <yhy.xmu@foxmail.com>
2017-08-24 20:03:30 +0800

    Update EventProducer.java

--- src/main/java/com/nowcoder/async/EventProducer.java
+++ src/main/java/com/nowcoder/async/EventProducer.java
@@ -7,7 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 /**
- * Created by nowcoder on 2016/7/16.
+ * Created by yhy on 2017/8/16.
  */
 @Service
 public class EventProducer {
